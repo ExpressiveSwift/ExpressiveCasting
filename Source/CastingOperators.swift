80eef76f1836d6df742d14b3f62248fcef3bb0cb
Andrey Tarantsov <andrey@tarantsov.com>
2015-09-30 05:07:10 +0600

    💊 Get rid of ~|||~

--- Source/CastingOperators.swift
+++ Source/CastingOperators.swift
@@ -29,90 +29,42 @@ import Foundation
 // "fuzzy cast",  aka  "make it so"
 postfix operator ~~~ {}
 
-// "fuzzy object OR",  aka  "make it so, or else"
-infix operator ~|||~ {}
-
 
 postfix public func ~~~ (optional: AnyObject?) -> Bool? {
     return BoolValue(optional)
 }
 
-public func ~|||~ (optional: AnyObject?, defaultValue: Bool) -> Bool {
-    return optional~~~ ?? defaultValue
-}
-
-
 postfix public func ~~~ (optional: AnyObject?) -> Int? {
     return IntValue(optional)
 }
-
-public func ~|||~ (optional: AnyObject?, defaultValue: Int) -> Int {
-    return optional~~~ ?? defaultValue
-}
-
 
 postfix public func ~~~ (optional: AnyObject?) -> Double? {
     return DoubleValue(optional)
 }
 
-public func ~|||~ (optional: AnyObject?, defaultValue: Double) -> Double {
-    return optional~~~ ?? defaultValue
-}
-
-
 postfix public func ~~~ (optional: AnyObject?) -> String? {
     return NonEmptyStringValue(optional)
 }
-
-public func ~|||~ (optional: AnyObject?, defaultValue: String) -> String {
-    return optional~~~ ?? defaultValue
-}
-
 
 postfix public func ~~~ (optional: AnyObject?) -> [Int]? {
     return ArrayValue(optional) { IntValue($0) }
 }
 
-public func ~|||~ (optional: AnyObject?, defaultValue: [Int]) -> [Int] {
-    return optional~~~ ?? defaultValue
-}
-
-
 postfix public func ~~~ (optional: AnyObject?) -> [String]? {
     return ArrayValue(optional) { StringValue($0) }
 }
-
-public func ~|||~ (optional: AnyObject?, defaultValue: [String]) -> [String] {
-    return optional~~~ ?? defaultValue
-}
-
 
 postfix public func ~~~ (optional: AnyObject?) -> JSONObject? {
     return JSONObjectValue(optional)
 }
 
-public func ~|||~ (optional: AnyObject?, defaultValue: JSONObject) -> [String: AnyObject] {
-    return optional~~~ ?? defaultValue
-}
-
-
 postfix public func ~~~ (optional: AnyObject?) -> [JSONObject]? {
     return JSONObjectsArrayValue(optional)
 }
 
-public func ~|||~ (optional: AnyObject?, defaultValue: [JSONObject]) -> [JSONObject] {
-    return optional~~~ ?? defaultValue
-}
-
-
 postfix public func ~~~ (optional: AnyObject?) -> NSURL? {
     return URLValue(optional)
 }
-
-public func ~|||~ (optional: AnyObject?, @autoclosure defaultValue: () -> NSURL) -> NSURL {
-    return optional~~~ ?? defaultValue()
-}
-
 
 postfix public func ~~~ <T: JSONObjectConvertible> (optional: AnyObject?) -> T? {
     return JSONConvertibleObjectValue(optional)
